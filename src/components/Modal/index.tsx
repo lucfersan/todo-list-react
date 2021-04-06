7acbd9189da25ec50be05ab85fb67e69cabec7c1
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-06 02:52:57 -0300

    feat: added connection to api with axios

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,14 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { ModalContext } from '../../contexts/Modal';
 
 import { Container, ModalContent, Form } from './styles';
 
-interface TodoProps {
-  id: number;
-  name: string;
-  done: boolean;
-}
+import api from '../../services/api';
 
 const Modal: React.FC = () => {
   const { closeModal, placeholder, id } = useContext(ModalContext);
 
   const [newTodo, setNewTodo] = useState('');
-  const [listTodos, setListTodos] = useState<TodoProps[]>(() => {
-    const storagedTodos = localStorage.getItem('@TodosReact:listTodos');
-
-    if (storagedTodos) {
-      return JSON.parse(storagedTodos);
-    }
-
-    return [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem('@TodosReact:listTodos', JSON.stringify(listTodos));
-  }, [listTodos]);
 
   const handleInputChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,30 +23,24 @@ const Modal: React.FC = () => {
 
       if (!newTodo) return;
 
-      const todo: TodoProps = {
-        id,
+      api.patch(`todos/${id}`, {
         name: newTodo,
-        done: false,
-      };
-
-      const filteredList = listTodos.filter(todo => todo.id !== id);
-      const updatedList = [...filteredList, todo];
-
-      localStorage.setItem(
-        '@TodosReact:listTodos',
-        JSON.stringify(updatedList),
-      );
+      });
 
       location.reload();
     },
-    [newTodo, listTodos],
+    [newTodo],
   );
 
   return (
     <Container>
       <ModalContent>
         <Form onSubmit={handleUpdateTodo}>
-          <input placeholder={placeholder} onChange={handleInputChange} />
+          <input
+            defaultValue={placeholder}
+            placeholder={placeholder}
+            onChange={handleInputChange}
+          />
 
           <div>
             <button type="button" onClick={closeModal}>
--- src/pages/Todos/index.tsx
+++ src/pages/Todos/index.tsx
@@ -6,6 +6,8 @@ import React, {
   useState,
 } from 'react';
 
+import api from '../../services/api';
+
 import { Container, Form, List, SpecialButton, TodoLi } from './styles';
 import { ModalContext } from '../../contexts/Modal';
 
@@ -13,6 +15,8 @@ interface TodoProps {
   id: number;
   name: string;
   done: boolean;
+  created_at: number;
+  updated_at: number;
 }
 
 const Todos: React.FC = () => {
@@ -20,19 +24,13 @@ const Todos: React.FC = () => {
 
   const inputRef = useRef<HTMLInputElement>(null);
   const [newTodo, setNewTodo] = useState('');
-  const [listTodos, setListTodos] = useState<TodoProps[]>(() => {
-    const storagedTodos = localStorage.getItem('@TodosReact:listTodos');
-
-    if (storagedTodos) {
-      return JSON.parse(storagedTodos);
-    }
-
-    return [];
-  });
+  const [listTodos, setListTodos] = useState<TodoProps[]>([]);
 
   useEffect(() => {
-    localStorage.setItem('@TodosReact:listTodos', JSON.stringify(listTodos));
-  }, [listTodos]);
+    api.get('todos').then(response => {
+      setListTodos(response.data);
+    });
+  }, []);
 
   const handleInputChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,10 +43,13 @@ const Todos: React.FC = () => {
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       if (!newTodo) return;
-      setListTodos(currentTodos => [
-        ...currentTodos,
-        { id: Date.now(), name: newTodo, done: false },
-      ]);
+      api
+        .post('todos', {
+          name: newTodo,
+        })
+        .then(response => {
+          setListTodos([...listTodos, response.data]);
+        });
 
       if (inputRef.current) {
         inputRef.current.value = '';
@@ -58,32 +59,14 @@ const Todos: React.FC = () => {
     [newTodo, listTodos],
   );
 
-  const handleRemove = useCallback(
-    todo => {
-      setListTodos(listTodos.filter(t => t !== todo));
-    },
-    [listTodos],
-  );
+  const handleRemove = useCallback((todo: TodoProps) => {
+    api.delete(`todos/${todo.id}`);
+    location.reload();
+  }, []);
 
   const handleTodoDone = useCallback(
     (id: number) => {
-      const getTodo = listTodos.filter(todo => todo.id === id);
-
-      const done = getTodo[0].done ? false : true;
-
-      const todo: TodoProps = {
-        ...getTodo[0],
-        done,
-      };
-
-      const filteredList = listTodos.filter(todo => todo.id !== id);
-      const updatedList = [...filteredList, todo];
-
-      localStorage.setItem(
-        '@TodosReact:listTodos',
-        JSON.stringify(updatedList),
-      );
-
+      api.patch(`todos/done/${id}`);
       location.reload();
     },
     [listTodos],
