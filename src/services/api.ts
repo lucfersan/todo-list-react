0f89837664a650de76dee57138a8692f96a033b5
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-07 18:02:45 -0300

    feat: added postgres url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    'postgres://txpdpipsfjcfgh:2e0620047bbef3c98e246488eb5c7eff4f0b746369583d2f5f3d27d24fbe6027@ec2-18-233-83-165.compute-1.amazonaws.com:5432/d7lqgcj3nin8do',
 });
 
 export default api;
