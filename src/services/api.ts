64a8f736844a670ef23da1128f74e0b46b41bfe7
Lucas Fernandes <fernandes.lucas11@outlook.com>
2021-04-08 10:23:48 -0300

    refactor: changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    'postgres://txpdpipsfjcfgh:2e0620047bbef3c98e246488eb5c7eff4f0b746369583d2f5f3d27d24fbe6027@ec2-18-233-83-165.compute-1.amazonaws.com:5432/d7lqgcj3nin8do',
+  baseURL: 'https://listlucas.herokuapp.com',
 });
 
 export default api;
